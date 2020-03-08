7d3a14d512b3e7997271cb6f54a73844cb1579b3
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-03-08 18:42:54 +0100

    backend chnages + monitoring

---
+++ nginx/bearer.lua
@@ -0,0 +1,20 @@
+local jwt = require "resty.jwt"
+
+local token = ngx.var.cookie_Authorization
+
+if token == nil then
+    ngx.status = 401
+    ngx.exit(401)
+end
+
+local validators = require "resty.jwt-validators"
+local claim_spec = {
+    exp = validators.is_not_expired()
+}
+
+local jwt_obj = jwt:verify("<jwtSecret>", token, claim_spec)
+if not jwt_obj["verified"] then
+    ngx.status = 401
+    ngx.log(ngx.WARN, jwt_obj.reason)
+    ngx.exit(401)
+end
