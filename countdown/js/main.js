49d92ba1cfbdbca62a4e4da31b9cb6f95f1c1d23
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-03-15 18:45:19 +0100

    countdown update

--- countdown/js/main.js
+++ countdown/js/main.js
@@ -1,16 +1,31 @@
-let clock = document.getElementById('clock');
+let d_el = document.getElementById('d');
+let h_el = document.getElementById('h');
+let m_el = document.getElementById('m');
+let s_el = document.getElementById('s');
+let date_el = document.getElementById('date');
 
 function startTime() {
-  var now = new Date();
-  var h = now.getHours();
-  var m = now.getMinutes();
-  var s = now.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  clock.innerHTML = `${h} : ${m} : ${s}`;
+  const now = new Date();
+  const finish = new Date(2020, 4, 1, 12);
+
+  var split = (finish.getTime() - now.getTime()) / 1000;
+
+  const d = Math.floor(split / (24 * 3600));
+  split = split % (24 * 3600);
+  const h = Math.floor(split / 3600);
+  split = split % 3600;
+  const m = Math.floor(split / 60);
+  split = split % 60;
+  const s = Math.floor(split);
+
+  d_el.innerHTML = format(d);
+  h_el.innerHTML = format(h);
+  m_el.innerHTML = format(m);
+  s_el.innerHTML = format(s);
+
   setTimeout(startTime, 1000);
 }
-function checkTime(i) {
+function format(i) {
   if (i < 10) {
     i = '0' + i;
   }
