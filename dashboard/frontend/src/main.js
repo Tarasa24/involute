a0cee87098c6f65d3d4e054bb58fa9c8a04d984a
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-03-22 23:26:16 +0100

    Traffic page draft

--- dashboard/frontend/src/main.js
+++ dashboard/frontend/src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 
 import App from './App.vue';
 import Home from './pages/Home';
+import Traffic from './pages/Traffic';
 import NotFound from './pages/NotFound';
 
 Vue.config.productionTip = false;
@@ -10,6 +11,7 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 const routes = [
   { path: '/', component: Home },
+  { path: '/traffic', component: Traffic },
   { path: '/*', component: NotFound },
 ];
 const router = new VueRouter({
---
+++ dashboard/frontend/src/pages/Traffic.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <div class="wrapper">
+      <Box title="Active connections" icon="fas fa-users" number="69420" />
+      <Box
+        title="Reading connections"
+        icon="fas fa-envelope-open-text"
+        number="69420"
+      />
+      <Box title="Writing connections" icon="fas fa-pen-alt" number="69420" />
+      <Box title="Idling connections" icon="fas fa-clock" number="69420" />
+    </div>
+
+    <iframe
+      @load="handleLoad"
+      :src="
+        process.env.NODE_ENV === 'production'
+          ? '/goaccess'
+          : 'http://localhost/goaccess'
+      "
+    />
+  </main>
+</template>
+
+<script>
+import Box from '../components/Box';
+
+export default {
+  components: { Box },
+  methods: {
+    handleLoad(event) {
+      const iframe = event.target;
+      iframe.style.height =
+        iframe.contentWindow.document.body.scrollHeight + 'px';
+      iframe.contentWindow.GoAccess.Nav.setTheme('bright');
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.wrapper
+  display: flex
+  padding: 0 3%
+  *
+    margin: auto
+
+main
+  padding: 2%
+
+iframe
+  border: 0
+
+  margin-top: 5%
+  width: 100%
+  // height is being replaced in production
+  height: 4000px
+</style>
