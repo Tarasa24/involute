bea9ee150816f5ae97e11f319de4c95a4827c10e
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-03-26 22:13:10 +0100

    dashboard | axios catch timeout

--- dashboard/backend/modules/socket.js
+++ dashboard/backend/modules/socket.js
@@ -5,28 +5,35 @@ function checkStatus(socket, status = {}) {
     process.env.NODE_ENV === 'production'
       ? 'http://nginx/nginx_status'
       : 'http://localhost/nginx_status';
-  axios.get(url).then(res => {
-    res = res.data.split('\n');
+  axios
+    .get(url)
+    .then(res => {
+      res = res.data.split('\n');
 
-    const thirdRow = res[3]
-      .replace(/[^0-9., ]+/g, '')
-      .slice(1, -1)
-      .split(' ');
-    const temp = {
-      active: Number(res[0].substring(res[0].search(':') + 1, res[0].length)),
-      reading: Number(thirdRow[0]),
-      writing: Number(thirdRow[2]),
-      idling: Number(thirdRow[4]),
-    };
-    if (JSON.stringify(status) !== JSON.stringify(temp)) {
-      status = temp;
-      socket.emit('status', JSON.stringify(status));
-    }
-  });
+      const thirdRow = res[3]
+        .replace(/[^0-9., ]+/g, '')
+        .slice(1, -1)
+        .split(' ');
+      const temp = {
+        active: Number(res[0].substring(res[0].search(':') + 1, res[0].length)),
+        reading: Number(thirdRow[0]),
+        writing: Number(thirdRow[2]),
+        idling: Number(thirdRow[4]),
+      };
+      if (JSON.stringify(status) !== JSON.stringify(temp)) {
+        status = temp;
+        socket.emit('status', JSON.stringify(status));
+      }
 
-  setTimeout(() => {
-    checkStatus(socket, status);
-  }, 1000);
+      setTimeout(() => {
+        checkStatus(socket, status);
+      }, 1000);
+    })
+    .catch(e => {
+      setTimeout(() => {
+        checkStatus(socket, status);
+      }, 10000);
+    });
 }
 
 module.exports = { checkStatus };
