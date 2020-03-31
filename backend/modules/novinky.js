e5a53bb7d4030d838182b8b7f7df731731b25361
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-03-31 22:14:36 +0200

    neighbors lookup edjusted to sorting by date

--- backend/modules/novinky.js
+++ backend/modules/novinky.js
@@ -2,17 +2,22 @@ const { ObjectId } = require('mongodb');
 
 async function neighbors(req, res, db) {
   try {
+    let current = await db
+      .collection('novinky')
+      .find({ _id: ObjectId(req.params.id) })
+      .next();
+
     let pervious = await db
       .collection('novinky')
-      .find({ _id: { $gt: ObjectId(req.params.id) } })
-      .sort({ _id: 1 })
+      .find({ date: { $gt: current.date } })
+      .sort({ date: 1, created: 1 })
       .project({ _id: true })
       .next();
 
     let next = await db
       .collection('novinky')
-      .find({ _id: { $lt: ObjectId(req.params.id) } })
-      .sort({ _id: -1 })
+      .find({ date: { $lt: current.date } })
+      .sort({ date: -1, created: -1 })
       .project({ _id: true })
       .next();
 
