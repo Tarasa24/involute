0589261c1c06fd30fdc1a0abf9eaf4a9a4b68717
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-04-02 23:07:11 +0200

    Update startDev.bat

--- startDev.bat
+++ startDev.bat
@@ -2,6 +2,7 @@ start cmd /k npm run serve --prefix .\frontend
 start cmd /k npm run serve --prefix .\backend
 
 start cmd /k npm run serve --prefix .\static
+start cmd /k npm run serve --prefix .\auth
 
 start cmd /k npm run serve --prefix .\dashboard\frontend
 start cmd /k npm run serve --prefix .\dashboard\backend
