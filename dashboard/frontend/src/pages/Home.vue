32583a68b31b09c61dcaeaf91a05bc5e11924869
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-04-07 16:17:54 +0200

    dashboard | Homepage

--- dashboard/frontend/src/pages/Home.vue
+++ dashboard/frontend/src/pages/Home.vue
@@ -1,9 +1,37 @@
 <template>
-  <main>Placeholder</main>
+  <main>
+    <div>
+      <img src="/img/logo.png" alt="logo" />
+      <h1>Vítejte, {{ name }}</h1>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+import { getTokenPayload } from '../assets/js/dataFetcher';
+
+export default {
+  data() {
+    return {
+      name: null,
+    };
+  },
+  async created() {
+    const { name } = await getTokenPayload();
+    this.name = name;
+  },
+};
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+main
+  display: grid
+  align-items: center
+
+img
+  max-width: calc( 100% - 20px )
+  max-height: 75vh
+  width: auto
+  height: auto
+  padding: 10px
+</style>
