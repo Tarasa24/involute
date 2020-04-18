5cd1d2b14a83ce91c1632666a1966a5046cf34ef
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-04-18 11:16:26 +0200

    new file compressor

--- dashboard/frontend/src/components/FileUpload.vue
+++ dashboard/frontend/src/components/FileUpload.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import imageCompression from 'browser-image-compression';
+import Compressor from 'compressorjs';
 
 export default {
   prop: ['value'],
@@ -13,11 +13,35 @@ export default {
     open() {
       this.$refs.uploadTrigger.click();
     },
+    toBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
+    },
     async handleUpload(event) {
       const file = event.target.files[0];
+      function compress(file) {
+        return new Promise((resolve, reject) => {
+          new Compressor(file, {
+            convertSize: 2500000,
+            maxWidth: 1920,
+            maxHeight: 1080,
+            success(result) {
+              resolve(result);
+            },
+            error(err) {
+              reject(err);
+            },
+          });
+        });
+      }
 
-      const scaled = await imageCompression(file, { maxSizeMB: 2 });
-      const base64 = await imageCompression.getDataUrlFromFile(scaled);
+      const compressed = await compress(file);
+      const base64 = await this.toBase64(compressed);
+
       this.$emit('input', base64);
     },
   },
