701c44fa070fbb7f9df91ddf43fce6906ecd9fca
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-05-12 00:07:00 +0200

    video background

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="home">
     <div class="head">
-      <span>
-        <h1>Nové <span>iNvolute</span> stránky?</h1>
-        <h1>Tady je máš!</h1>
-        <h2>
-          Zase změna, ale teď je to tak, jak jsme chtěli. Product placement
-          nechceme, na novinky z her jsou jiné portály, tak tady vidíš, kdo jsme
-          prostě MY!
-        </h2>
-      </span>
+      <video
+        v-if="width > 767"
+        src="/video/involuteWeb.mp4"
+        poster="/img/thumb/involuteWeb.png"
+        autoplay
+        muted
+        loop
+      />
+      <img v-else src="/img/thumb/involuteWeb.png" alt="involuteWeb.png" />
+      <div class="text">
+        <span>
+          <h1>Nové <strong>iNvolute</strong> stránky?</h1>
+          <h1>Tady je máš!</h1>
+          <h2>
+            Zase změna, ale teď je to tak, jak jsme chtěli. Product placement
+            nechceme, na novinky z her jsou jiné portály, tak tady vidíš, kdo
+            jsme prostě MY!
+          </h2>
+        </span>
+      </div>
     </div>
 
     <Sponsors />
@@ -25,24 +36,66 @@ import Media from './Media.vue';
 
 export default {
   components: { Sponsors, News, Media },
+  data() {
+    return {
+      width: window.innerWidth,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth;
+    });
+  },
+  unmounted() {
+    window.removeEventListener('resize');
+  },
 };
 </script>
 
 <style lang="sass" scoped>
 .head
   text-align: left
-  background-color: #02021f
   color: $textGray
+  position: relative
+  text-align: center
+  width: 100%
+  height: 90vh
+  overflow: hidden
+  &::before
+    content: ''
+    background: rgba(2, 2, 31, 0.4)
+    width: 100%
+    height: 100%
+    position: absolute
+    left: 0
+    top: 0
+    z-index: 1
+
+  video, img
+    min-height: 100%
+    width: auto
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+
+.text
+  width: 100%
+  position: absolute
+  left: 50%
+  top: 50%
+  transform: translate(-50%, -50%)
   display: grid
   justify-items: center
-  align-items: center
-  padding: 15% 0
+  z-index: 2
   @include outside-boundaries
     padding-left: 2.5%
+    width: 100% - 2.5%
   @include medium-device
     padding-left: 5%
-
+    width: 100% - 5%
   span
+    text-align: left
     width: $baselineWidth
     max-width: $maxWidth
     @include medium-device
@@ -52,7 +105,7 @@ export default {
     font-size: 3.5rem
     letter-spacing: 5px
     margin: 2% 0
-    span
+    strong
       color: $purple
     @include medium-device
       font-size: 3rem
