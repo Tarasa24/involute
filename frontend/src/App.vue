32c28152de40853ead4caecad742114d7ae70718
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-05-13 19:13:43 +0200

    google analytics prep

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -27,6 +27,15 @@ export default {
     init(cookies) {
       if (cookies.tracking) {
         //load tracking scripts
+        this.loadScript(
+          null,
+          "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
+            '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
+            'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
+            "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');" +
+            `ga('create', '${process.env.GA_MEASUREMENT_ID}', 'auto');` +
+            "ga('send', 'pageview');"
+        );
       }
       if (cookies.marketing) {
         //load markteting scripts
