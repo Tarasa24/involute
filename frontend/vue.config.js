f9fcf6e7c6e6f8290bebb4580da8bed008145175
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-05-19 15:39:20 +0200

    frontend compression

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
+  productionSourceMap: false,
   css: {
     loaderOptions: {
       sass: {
@@ -16,10 +17,13 @@ module.exports = {
           NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         },
       }),
+      new CompressionPlugin({
+        include: /\.(html|css|js|ico|svg)$/,
+        minRatio: 0.9,
+      }),
     ],
   },
   chainWebpack(config) {
     config.plugins.delete('prefetch');
-    config.plugin('CompressionPlugin').use(CompressionPlugin);
   },
 };
