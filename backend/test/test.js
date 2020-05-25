670eff9ccb500d9ee9f5bd61d9a3a1f7aa5991f8
Tarasa24 <petr.gajdosik2424@gmail.com>
2020-05-25 21:08:40 +0200

    remove tests

--- backend/test/test.js
+++
@@ -1,7 +0,0 @@
-describe('Mongodb connection', () => {
-  it('Mongodb url variable in production should point to the mongodb container', () => {
-    process.env.NODE_ENV = 'production';
-    let url = require('../modules/credentials').url;
-    expect(url).toBe('mongodb://mongodb:27017');
-  });
-});
--- frontend/test/test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-test('Base url for api requests should be /api', () => {
-  process.env.NODE_ENV = 'production';
-  let baseUrl = require('../src/assets/js/dataFetcher');
-
-  console.log(process.env.PROD);
-  expect(baseUrl).toBe('/api');
-});
